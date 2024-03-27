51e90c8015a36b9bfba9810d90b5d2d6689d823a
elilaird <elaird03@outlook.com>
2024-03-27 18:35:44 -0500

    updated pipeline interface with get_data iterator

--- smartcadd/pipeline.py
+++ smartcadd/pipeline.py
@@ -1,6 +1,8 @@
-from typing import Any, List
+from typing import Any, List, Iterable
 from .data import Compound
 from .filters import Filter
+from .dataset import IterableDataset
+
 
 class Pipeline:
     """
@@ -9,25 +11,42 @@ class Pipeline:
     Args:
         filters (List[Filter]): list of Filters to apply to the Compounds
     """
-    def __init__(self, filters: List[Filter]):
+
+    def __init__(self, data_loader: IterableDataset, filters: List[Filter]):
+        self.data_loader = data_loader
         self.filters = filters
 
-    def __call__(self, compounds: List[Compound], *args: Any, **kwargs: Any) -> Any:
+    def __call__(
+        self, compounds: List[Compound], *args: Any, **kwargs: Any
+    ) -> Any:
         return self.run(compounds, *args, **kwargs)
 
-    def run(self, compounds: List[Compound], *args: Any, **kwargs: Any) -> Any:
-        return NotImplementedError("This method should be implemented in the subclass.")
+    def run_filters(
+        self, compounds: List[Compound], *args: Any, **kwargs: Any
+    ) -> Any:
+        return NotImplementedError(
+            "This method should be implemented in the subclass."
+        )
 
+    def get_data(self) -> Iterable[Compound]:
+        return NotImplementedError(
+            "This method should be implemented in the subclass."
+        )
 
 
 class BasicCompoundPipeline(Pipeline):
     """
     Basic implementation of a Compound Pipeline
     """
+
     def __init__(self, filters: List[Filter]):
         super().__init__(filters)
 
-    def run(self, compounds: List[Compound]) -> List[Compound]:
+    def run_filters(self, compounds: List[Compound]) -> List[Compound]:
         for filter in self.filters:
             compounds = filter(compounds)
         return compounds
+
+    def get_data(self) -> Iterable[Compound]:
+        for batch in self.data_loader:
+            yield batch
